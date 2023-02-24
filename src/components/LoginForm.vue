2c5c2f06c6c8aedffa522d9e266a119cf72a3310
Hubert Claesson <112488638+HubertClaesson@users.noreply.github.com>
2023-02-24 14:41:11 +0100

    Revert "Kevin lane"

--- src/components/LoginForm.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h1>Sign in</h1>
-  <input type="email" placeholder="Email Address">
-  <input type="password" placeholder="Password">
-  <a href="">Create an account</a>
-  <button>Login</button>
-</template>
