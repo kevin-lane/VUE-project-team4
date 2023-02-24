24b923431be1b8f699fde9fedea5f226bf69a665
Hubert Claesson <hubert.claeson@gmail.com>
2023-02-24 15:30:49 +0100

    Search form in progress
    Search form in progress

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="div-container">
+    <div id="form-container">
+      <h2>Welcome to InstaBuy</h2>
+      <form action="">
+        <label for="search-label">Search</label>
+        <input type="text" name="search-box" id="search-box" placeholder="sök" />
+        <label for="location">Choose location</label>
+        <select name="city" id="cities">
+          <option value="stockholm">Stockholm</option>
+          <option value="liljeholmen">Liljeholmen</option>
+          <option value="hornstull">Hornstull</option>
+        </select>
+        <input type="submit" value="Find ad" id="send" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h2{
+    font-size: 24px;
+    font-weight: 700;
+    color: rgb(79, 117, 152, 1);
+    text-align: center;
+}
+
+label{
+    display: block;
+    margin-top: 1em;
+    font-weight: lighter;
+}
+
+#send{
+    display: block;
+    margin-bottom: 2em;
+    margin-top: 2em;
+    background-color: rgb(79, 117, 152, 1);
+    color: aliceblue;
+    background-color: rgb(79, 117, 152, 1);
+    border: rgb(79, 117, 152, 1);
+    width: 12em;
+    height: 2.4em;
+    border-radius: 4px;
+}
+
+#form-container{
+    background-color: rgb(207, 219, 219);
+    padding: 1em;
+    width: 250px;
+    height:auto;
+    border-radius: 1em;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+
+form{
+   display: flex;
+   justify-content: center;
+   flex-direction: column;
+   margin-left: 19%;
+}
+
+#div-container{
+    display: flex;
+    justify-content: center;
+    margin-top: 4em;
+}
+
+#search-box{
+    width: 12em;
+}
+#cities{
+    width: 12.5em;
+    padding: 0.1em;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,16 @@
+<script setup>
+
+import { SearchForm } from '../components/SearchForm.vue';
+
+</script>
+
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+
+  <SearchForm />
 </template>
 
 <style>
