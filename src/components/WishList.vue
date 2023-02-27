2b65311ff7a73a171b96004acb9c920bce4f6e7c
Oskarnn <112063723+Oskarnn@users.noreply.github.com>
2023-02-27 14:10:31 +0100

    Wishlist
    Created wishlist - ongoing

---
+++ src/components/WishList.vue
@@ -0,0 +1,49 @@
+<script></script>
+
+<template>
+    <section class="wishlist-card">
+        <div class="head">
+            <h2>Wishlist</h2>
+        </div>
+        <div class="grid">
+            <div class="div-card">
+                <img src="" alt="">
+                <h3>$18.00</h3>
+            </div>
+            <div class="div-card">
+                <img src="" alt="">
+                <h3>$18.00</h3>
+            </div>
+            <div class="div-card">
+                <img src="" alt="">
+                <h3>$18.00</h3>
+            </div>
+            <div class="div-card">
+                <img src="" alt="">
+                <h3>$18.00</h3>
+            </div>
+            <div class="div-card">
+                <img src="" alt="">
+                <h3>$18.00</h3>
+            </div>
+        </div>
+
+    </section>
+</template>
+
+<style scoped>
+
+.wishlist-card{
+    width: 730px;
+}
+.head{
+    font-size: 24px;
+    font-weight: 600;
+}
+
+.grid{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+}
+</style>
