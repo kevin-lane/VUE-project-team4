8a438d6f129a296e1b55881836b23838cfdd3843
MartinITHS <martin@emsound.se>
2023-02-28 16:21:26 +0100

    Create NavbarMobile.vue

---
+++ src/components/NavbarMobile.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="navbar">
+        <a class="active" href="#"><i class="fa fa-fw fa-home"></i> </a>
+        <a href="#"><i class="fa fa-fw fa-search"></i> </a>
+        <a href="#"><i class="fa-solid fa-plus"></i> </a>
+        <a href="#"><i class="fa fa-fw fa-user"></i> </a>
+    </nav>
+</template>
+
+<script src="https://kit.fontawesome.com/315f7034a9.js" crossorigin="anonymous"></script>
+
+
+<style scoped>
+body {
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.navbar {
+    overflow: hidden;
+    background-color: #ffffff;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 1.5vh;
+}
+
+.navbar a {
+    display: inline-block;
+    color: #2c2c2c;
+    padding: 16px 25px;
+    text-decoration: none;
+    font-size: 17px;
+    text-align: center;
+}
+</style>
