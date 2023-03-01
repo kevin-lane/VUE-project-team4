977ecdd30922681c675055a23abeeefd43b1f2e2
Oskarnn <112063723+Oskarnn@users.noreply.github.com>
2023-03-01 14:50:13 +0100

    PostAdd
    PostAdd finished for review

--- src/components/PostAdd.vue
+++ src/components/PostAdd.vue
@@ -20,14 +20,16 @@
         </div>
         <div id="form-pic">
             <h3>Lägg till bild:</h3>
-            <input type="text">
+            <input type="file" accept="image/*">
         </div>
         <div id="form-cap">
             <h3>Beskrivning:</h3>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <textarea cols="30" rows="5">Beskriv din produkt...</textarea>
         </div>
     </div>
-
+    <div class="div-btn">
+        <input type="button" id="btn" value="Post">
+    </div>
 
   </section>
 </template>
@@ -36,11 +38,11 @@
     .post-add{
         width: 730px;
         background-color: #FFFFFF;
-
-        margin: auto;
         border-radius: 7px;
         box-shadow: 1px 2px 2px  rgb(205, 204, 204);
+        margin: auto;
         margin-top: 15vh;
+        padding-top: 20px;
     }
 
     h2{
@@ -50,6 +52,61 @@
     .form-grid{
         display: grid;
         grid-template-columns: repeat(2, 1fr);
+        /* border: solid black 1px; */
+        padding: 10px 50px 0px 50px;
+
     }
 
+    #form-cat  {
+        margin-bottom: 15vh;
+
+    }
+    #form-cat > select {
+        width: 219px;
+        height: 39px;
+        background-color: #d9d9d9;
+        border: none;
+        border-radius: 6px;
+
+    }
+
+    #form-price > input {
+        width: 219px;
+        height: 39px;
+        background-color: #d9d9d9;
+        border: none;
+        border-radius: 6px;
+
+    }
+
+    #form-cap > textarea {
+        background-color: #d9d9d9;
+        border: none;
+        border-radius: 6px;
+
+    }
+    .line {
+        display: block;
+        width: 692px;
+        height: 1px;
+         background-color: #d9d9d9;
+         margin: 35px auto 3px auto;
+    }
+
+    #btn {
+        margin: 0 auto;
+        background-color: #4F7598;
+        border: none;
+        border-radius: 6px;
+        color: white;
+    width: 204px;
+    height: 54px;
+
+    }
+    .div-btn {
+
+        text-align: center;
+        padding: 25px;
+
+    }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import PostAdd from '../components/PostAdd.vue';
+
 </script>
 
 <template>
   <main>
-   <PostAdd/>
   </main>
 </template>
