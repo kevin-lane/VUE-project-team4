4bf8873f6554fc56aed7bfc55a776d45252d1b04
Oskarnn <112063723+Oskarnn@users.noreply.github.com>
2023-03-01 10:12:59 +0100

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import LoginForm from '../components/LoginForm.vue'
+import WishList from '../components/WishList.vue'
 </script>
 
 <template>
   <main>
+    <WishList/>
   </main>
 </template>
