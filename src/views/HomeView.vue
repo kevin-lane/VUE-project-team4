faff116dfb0670cc68e299cee0a8f536fb450d15
MartinITHS <martin@emsound.se>
2023-03-01 09:21:06 +0100

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,12 @@
 <script setup>
-<<<<<<< Updated upstream
 import NavbarMobile from '../components/NavbarMobile.vue'
-=======
-import Navbar from '../components/Navbar.vue'
->>>>>>> Stashed changes
 </script>
 
 <template>
   <main>
-<<<<<<< Updated upstream
     <NavbarMobile />
-=======
+
     <Navbar />
->>>>>>> Stashed changes
+
   </main>
 </template>
