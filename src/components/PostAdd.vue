5325d39461b11d7d051a97164870e0db859bfd02
Oskarnn <112063723+Oskarnn@users.noreply.github.com>
2023-03-01 16:59:55 +0100

    PostAdd complete
    PostAdd complete and fixed HeroSection css

--- src/components/PostAdd.vue
+++ src/components/PostAdd.vue
@@ -4,7 +4,7 @@
     <h2>Lägg till annons</h2>
     <span class="line"></span>
     <div class="form-grid">
-        <div id="form-cat">
+        <div class="form" id="form-cat">
             <h3>Välj kategori:</h3>
             <select>
                 <option>Kläder</option>
@@ -14,15 +14,15 @@
                 <option>Hälsa</option>
             </select>
         </div>
-        <div id="form-price">
+        <div class="form" id="form-price">
             <h3>Pris:</h3>
             <input type="text">
         </div>
-        <div id="form-pic">
+        <div class="form" id="form-pic">
             <h3>Lägg till bild:</h3>
             <input type="file" accept="image/*">
         </div>
-        <div id="form-cap">
+        <div class="form" id="form-cap">
             <h3>Beskrivning:</h3>
             <textarea cols="30" rows="5">Beskriv din produkt...</textarea>
         </div>
@@ -109,4 +109,66 @@
         padding: 25px;
 
     }
+
+
+    @media screen and (max-width: 720px) {
+
+        .post-add{
+        width: 100%;
+        background-color: #FFFFFF;
+        border-radius: 7px;
+        box-shadow: 1px 2px 2px  rgb(205, 204, 204);
+        margin-top: 10vh;
+        padding-top: 5px;
+    }
+
+        .form-grid{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+
+    }
+
+    .form {
+        margin-top: 3vh;
+    }
+
+    #form-cat  {
+        margin-bottom: 0vh;
+        margin-right: 55px;
+
+    }
+
+    #form-price {
+     margin-right: 55px;
+
+    }
+
+    #form-pic {
+        margin-left: 80px;
+    }
+    .line {
+        width: 90%;
+         background-color: #d9d9d9;
+         margin: 15px auto 3px auto;
+    }
+
+    #btn {
+        margin: 0 auto;
+        background-color: #4F7598;
+        border: none;
+        border-radius: 6px;
+        color: white;
+    width: 204px;
+    height: 54px;
+
+    }
+    .div-btn {
+
+        text-align: center;
+
+    }
+
+}
 </style>
