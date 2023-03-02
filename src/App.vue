39c4b31462f27d76a86a4c23dd7186df9a73fc5f
Kevin <kevinlane3@hotmail.com>
2023-03-02 11:55:03 +0100

    Popup window is done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import HeroSection from './components/HeroSection.vue'
 import NavBar from './components/NavBar.vue'
+import PopupWindow from './components/PopupWindow.vue';
 </script>
 
 <template>
   <NavBar />
   <HeroSection/>
+  <PopupWindow />
+
 </template>
 
 <style scoped></style>
