d28659692a5d03d8a84e5845c52590ab5517a752
Oskarnn <112063723+Oskarnn@users.noreply.github.com>
2023-03-03 10:25:39 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import NavbarMobile from './components/NavbarMobile.vue'
+import HeroSection from './components/HeroSection.vue'
 </script>
 
 <template>
      <NavbarMobile />
-
+     <HeroSection/>
 <RouterView />
 </template>
 
