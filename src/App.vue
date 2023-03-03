e6844fe89fe6d9b45209258c10358999425325b2
Hubert Claesson <hubert.claeson@gmail.com>
2023-03-03 09:57:04 +0100

    components done (6) uppdated

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<script setup>
-  import { RouterLink, RouterView } from 'vue-router';
-</script>
-
 <template>
-  <RouterView />
+  <main></main>
 </template>
-
-<style scoped>
-
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,3 @@
-<script setup>
-import SearchForm from '../components/SearchForm.vue';
-import CartPaymentMobile from '../components/CartPaymentMobile.vue';
-import PaymentDenial from '../components/PaymentDenial.vue';
-import PaymentAproved from '../components/PaymentAproved.vue';
-import ProfilePage from '../components/ProfilePage.vue';
-</script>
-
 <template>
-  <main>
-    <h1>Instabuy</h1>
-    <SearchForm />
-    <CartPaymentMobile />
-    <PaymentDenial />
-    <PaymentAproved />
-    <ProfilePage />
-  </main>
+  <main></main>
 </template>
