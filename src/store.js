73788cd623a73dccf716efa437619cbdadbb140c
Kevin <kevinlane3@hotmail.com>
2023-03-15 19:12:11 +0100

    User ID added to each user and can be handled for logged in user

--- src/store.js
+++ src/store.js
@@ -8,31 +8,12 @@ const mutations = {
     state.loggedIn = loggedIn;
     state.loggedInUserId = loggedInUserId;
     console.log(state.loggedInUserId);
-  },
-
-  storeWish(state, product){
-  state.productPicture.push(product.picture);
-  state.productPrice.push(product.price);
-  state.wishClass = true;
-  },
-  removeWish(state, index) {
-    state.productPicture.splice(index, 1);
-  state.productPrice.splice(index, 1);
-
-  if(state.productPrice.length < 1){
-    state.wishClass = false;
-    }
   }
-};
-
-
-const state = {
+},
+state = {
   newUser: false,
   loggedIn: false,
-  loggedInUserId: null,
-    productPicture: [],
-  productPrice: [],
-  wishClass: false
+  loggedInUserId: 0
 }
 
 //Uses strict: true
